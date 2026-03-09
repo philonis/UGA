9ff9df278b705d6c5de86f5dcd6271d3bb03da5d
Philonis <philonis@gmail.com>
2026-03-09 11:00:01 +0800

    Deploy to GitHub Pages

--- frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cdn.cloudflare.steamstatic.com', 'avatars.steamstatic.com'],
-  },
-}
-
-module.exports = nextConfig
